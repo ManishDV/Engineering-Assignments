d930a7f6e8c60fe4de2ade5a436c87814a5851df
Manish Dnyaneshwar Visave <43295558+ManishDV@users.noreply.github.com>
2020-01-28 12:08:51 +0530

    Add files via upload

--- TE-SEM2-Assigns/DAA & SP/Optimal Storage On Tape/ost.cpp
+++ TE-SEM2-Assigns/DAA & SP/Optimal Storage On Tape/ost.cpp
@@ -69,7 +69,13 @@ int Sort(int p,int r)
     }
 }
 
-void storeOnTape(int n){
+void storeOnTape(){
+	int n=0;
+	cout<<"\nPlease Enter The Size Of Array: ";
+	cin>>n;
+
+	inputArray(n);
+	Sort(0,n-1);
 	int tapeSize = 0;
 	cout<<"\n================================";
 	cout<<"\nEnter The Capacity Of Tape: ";
@@ -119,15 +125,151 @@ void storeOnTape(int n){
 
 }
 
-int main(int argc, char const *argv[])
-{	
-
-	int n = 0;
+void multiStorage(int mode){
+	int n=0;
 	cout<<"\nPlease Enter The Size Of Array: ";
 	cin>>n;
 
 	inputArray(n);
 	Sort(0,n-1);
-	storeOnTape(n);
+	int tapeSize = 0;
+	int tapeCount = 0;
+	cout<<"\n================================";
+	cout<<"\nEnter The Number Of Tapes: ";
+	cin>>tapeCount;
+	cout<<"================================\n";
+
+	int tapeSizes[tapeCount];
+	for(int i=0;i<tapeCount;i++){
+		cout<<"\nEnter The Capacity Of Tape "<<i+1<<" : ";
+		cin>>tapeSizes[i];
+	}
+
+	if(mode == 1){
+		int total = 0;
+		tapeIdx = 0;
+		int start = 0;
+		int idxArr[tapeCount]; 
+		struct programs tapes[tapeCount][20];
+		for(int i=0;i<tapeCount;i++){
+			start = tapeIdx; 
+			total = 0;
+			tapeIdx = 0;
+							
+			for(int j=start;j<n;j++){
+				total += program[j].length;
+				if(tapeSizes[i] >= total){
+						tapes[i][tapeIdx] = program[j];
+						tapeIdx++;
+				}else{
+					start = tapeIdx;
+					idxArr[i] = tapeIdx;
+					break;
+				}
+			}
+		}
+		total = 0;
+		float sumArr[tapeCount];
+		for(int i=0;i<tapeCount;i++){
+			
+			cout<<"\nLENGTH OF TAPE "<<i+1<<" IS "<<idxArr[i];
+			for(int j=0;j<idxArr[i];j++){
+				int sum = 0;
+					if(j==0){
+						total = tapes[i][j].length;
+					}else{
+						for(int k=0;k<=j;k++){
+							sum+=tapes[i][k].length;
+						}
+					}	
+						total = sum+total;
+			}
+				cout<<"\nTOTAL OF TAPE "<<i+1<<" Is: "<<total;
+				sumArr[i] = float(total) / float(idxArr[i]);
+		}
+
+		for (int i = 0; i < tapeCount; ++i)
+		{
+
+			cout<<"\n===========================================\n";
+			cout<<"\t\tTAPE "<<i+1<<" CONTENT";
+			cout<<"\n===========================================\n";
+			for (int j = 0; j < idxArr[i]; ++j)
+			{
+
+				cout<<"P"<<tapes[i][j].program_no<<"\t";
+			}
+
+			cout<<"\n===========================================\n";
+			cout<<"\n===========================================\n";
+			cout<<"\nTOTAL MRT IS: "<<sumArr[i]<<"ms";
+			cout<<"\n===========================================\n";
+			
+		}	
+			
+		
+
+
+	}else{
+
+	}
+
+}
+
+int main(int argc, char const *argv[])
+{	
+
+	int n = 0;
+	int choice = 0;
+	int choice1 = 0;
+
+	do{
+		cout<<"\n===================================\n";
+		cout<<"\n\t\tMENU\n";
+		cout<<"\n===================================\n";
+		cout<<"\n\t1.Single Tape";
+		cout<<"\n\t2.Multi Tape";
+		cout<<"\n\t3.Exit";
+		cout<<"\n===================================\n";
+		
+		cout<<"Enter Your Choice: ";
+		cin>>choice;
+
+		switch(choice){
+			case 1:
+					storeOnTape();
+					break;
+			case 2:
+					
+					do{
+						cout<<"\n===================================\n";
+						cout<<"\n\t\tMENU\n";
+						cout<<"\n===================================\n";
+						cout<<"\n\t1.Without Round Robin.";
+						cout<<"\n\t2.With Round Robin.";
+						cout<<"\n\t3.Go To Main Menu.";
+						cout<<"\n===================================\n";
+						
+						cout<<"Enter Your Choice: ";
+						cin>>choice1;
+				
+						switch(choice1){
+							case 1:
+									multiStorage(1);
+									break;
+							case 2:
+									break;		
+							case 3:
+									break;
+						}
+					}while(choice1 != 3);
+					break;
+			case 3:
+					return 0;
+					
+		}
+		
+	}while(choice != 3);
+
 	return 0;
 }
