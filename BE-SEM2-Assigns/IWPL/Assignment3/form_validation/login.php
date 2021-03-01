3f034355e15a090b7af5745d0003a4945c4cf30f
Manish Visave <manishvisave149@gmail.com>
2021-03-01 12:29:13 +0530

    AJAX done

--- BE-SEM2-Assigns/IWPL/Assignment3/form_validation/login.php
+++ BE-SEM2-Assigns/IWPL/Assignment3/form_validation/login.php
@@ -8,9 +8,17 @@ error_reporting(0);
 
 <head>
     <title>Login to Music Gallary</title>
+    
+    <script src="js/jquery.js"></script>
     <link rel="stylesheet" type="text/css" href="css/navbarstyle.css">
     <link rel="stylesheet" type="text/css" href="css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        .hidden{
+            display:none;
+            color: red;
+        }
+    </style>
 </head>
 
 
@@ -50,8 +58,10 @@ error_reporting(0);
             </div>
             <div class="fields">
                 <form method="POST" action="validate.php">
-                    <input type="text" name="username" id="username" placeholder="username" align="center" autocomplete="off" />
-                    <input type="password" name="pass" id="pass" placeholder="Password" autocomplete="off">
+                    <input type="text" name="username" id="username" placeholder="username" align="center" autocomplete="off" required/>
+                    <center><span id="userError" class="hidden"></span></center>
+                    <input type="password" name="pass" id="pass" placeholder="Password" autocomplete="off" required/>
+                    <center><span id="passError" class="hidden"></span></center>
                     <input type="submit" value="Login" id="btnSubmit">
                     <div class="forgotPass"><a href="forgotPass.php">Forgot Password</a></div>    
                 </form>
@@ -63,7 +73,81 @@ error_reporting(0);
         </div>
     </div>
 </body>
+<script>
 
+    $("document").ready(function(){
+        $("#username").focusout(function(){
+            let usr = $("#username").val();
+            if(usr.length > 0){
+                $.ajax({ 
+                type: "POST", 
+                url: "check.php", 
+                data: "username="+ usr,
+                dataType: 'text',
+                    success: function(msg){
+                        if(msg == 'EXISTS'){
+                            $("#username").css('border-bottom','1px solid green');
+                           
+                            $("#userError").slideUp("slow");
+                        } else {
+                            $("#username").css('border-bottom','1px solid red');
+                            $("#userError").html("No user present with username <b>" + usr + "</b>");
+                            $("#userError").slideDown("slow");
+                        }
+                    }
+
+                });
+            }else{
+
+                $("#username").css('border-bottom','1px solid red');
+                $("#userError").html("This field is necessary");
+                $("#userError").slideDown("slow");
+                
+            }
+            
+        });
+        $("#pass").keyup(function(){
+            let usr = $("#username").val();
+            let pass = $("#pass").val();
+            let jsonObj = {"username":usr,"pass":pass};
+            
+            if(pass.length > 0 && usr.length > 0){
+                $.ajax({ 
+                type: "POST", 
+                url: "check.php", 
+                data: {data: JSON.stringify( jsonObj )},
+                dataType: 'text',
+                    success: function(msg){
+                        console.log(msg);
+                        
+                        if(msg == 'EXISTS'){
+                            $("#pass").css('border-bottom','1px solid green');
+                            $("#passError").slideUp("slow");
+                        } else {
+                            $("#passError").slideUp("slow");
+                            $("#pass").css('border-bottom','1px solid red');
+                            $("#passError").html("Incorrect Password");
+                            $("#passError").slideDown("slow");
+                        }
+                    },
+                    error: function(msg){
+                        // var err = eval("(" + msg.responseText + ")");
+                        console.log(msg.responseJSON);
+                    }
+
+                });
+            }else{
+                $("#pass").css('border-bottom','1px solid red');
+                $("#passError").html('This field is necessary');
+                
+                $("#passError").slideDown("slow");
+                
+            }
+            
+        });
+    });
+
+</script>
 </html>
 
 <?php
